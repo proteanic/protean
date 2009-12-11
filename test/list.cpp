5bc01c44d9c392d5a1d7ac42f2e7ad7fdbbfede1
Johan Ditmar <johanditmar@gmail.com>
2009-12-11 23:29:49 +0000

    Add unit tests for list (re #5)

---
+++ test/list.cpp
@@ -0,0 +1,132 @@
+//  (C) Copyright Johan Ditmar, Paul O'Neill & Luke Stedman 2009.
+//  Use, modification and distribution are subject to the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt).
+
+#include <boost/test/test_tools.hpp>
+#include <boost/test/unit_test.hpp>  
+
+using boost::unit_test::test_suite;    
+
+#include <protean/variant.hpp>
+using namespace protean;
+
+void test_List()
+{
+    variant v1(variant::List);
+    BOOST_CHECK(v1.is<variant::List>());
+    BOOST_CHECK(v1.is<variant::Sequence>());
+    BOOST_CHECK(!v1.is<variant::Mapping>());
+    BOOST_CHECK(!v1.is<variant::Primitive>());
+   
+    BOOST_CHECK(v1.empty());
+    BOOST_CHECK_EQUAL(v1.size(), 0);
+
+    boost::int32_t arg1(0xFFFFFFFF);
+    std::string arg2("This is a string");
+
+    v1.push_back(variant(arg1));
+    v1.push_back(variant(arg2));
+
+    BOOST_CHECK(!v1.empty());
+    BOOST_REQUIRE(v1.size()==2);
+
+    BOOST_CHECK(v1[0].is<boost::int32_t>());
+    BOOST_CHECK_EQUAL(v1[0].as<boost::int32_t>(), arg1);
+    BOOST_CHECK(v1[1].is<std::string>());
+    BOOST_CHECK_EQUAL(v1[1].as<std::string>(), arg2);
+
+    // test equality
+    variant v2(variant::List);
+    BOOST_CHECK(v1!=v2);
+
+    v2.push_back(variant(arg1));
+    v2.push_back(variant(arg2));
+    BOOST_CHECK(v1==v2);
+
+    variant v3(v2);
+    BOOST_CHECK(v3.is<variant::List>());
+    BOOST_CHECK(v3==v2);
+
+    variant v4;
+    v4 = v3;
+    BOOST_CHECK(v4.is<variant::List>());
+    BOOST_CHECK(v4==v3);
+}
+
+void test_list_iterator()
+{
+    variant v1(variant::List);
+
+    BOOST_CHECK(v1.begin()==v1.end());
+
+    boost::int32_t arg1(0xFFFFFFFF);
+    std::string arg2("This is a string");
+
+    v1.push_back(variant(arg1));
+    v1.push_back(variant(arg2));
+
+    BOOST_CHECK(v1.begin()!=v1.end());
+
+    variant::const_iterator const_begin(v1.begin());
+    variant::const_iterator const_end(v1.end());
+
+    BOOST_CHECK_THROW(const_begin.key(), variant_error);
+    BOOST_CHECK_THROW(const_begin.time(), variant_error);
+
+    BOOST_CHECK(const_begin->is<boost::int32_t>());
+    BOOST_CHECK_EQUAL(const_begin->as<boost::int32_t>(), arg1);
+
+    ++const_begin;
+    BOOST_CHECK(const_begin->is<std::string>());
+    BOOST_CHECK_EQUAL(const_begin->as<std::string>(), arg2);
+
+    ++const_begin;
+    BOOST_CHECK(const_begin==const_end);
+}
+
+void test_list_const_iterator()
+{
+    variant v1(variant::List);
+
+    BOOST_CHECK(v1.begin()==v1.end());
+
+    boost::int32_t arg1(0xFFFFFFFF);
+    std::string arg2("This is a string");
+
+    v1.push_back(variant(arg1));
+    v1.push_back(variant(arg2));
+
+    BOOST_CHECK(v1.begin()!=v1.end());
+
+    variant::iterator begin(v1.begin());
+    variant::iterator end(v1.end());
+
+    BOOST_CHECK_THROW(begin.key(), variant_error);
+    BOOST_CHECK_THROW(begin.time(), variant_error);
+
+    BOOST_CHECK(begin->is<boost::int32_t>());
+    BOOST_CHECK_EQUAL(begin->as<boost::int32_t>(), arg1);
+    BOOST_CHECK_EQUAL(begin.value().as<boost::int32_t>(), arg1);
+
+    *begin = arg2;
+    BOOST_CHECK(begin->is<std::string>());
+    BOOST_CHECK_EQUAL(begin->as<std::string>(), arg2);
+
+    ++begin;
+    BOOST_CHECK(begin->is<std::string>());
+    BOOST_CHECK_EQUAL(begin->as<std::string>(), arg2);
+
+    ++begin;
+    BOOST_CHECK(begin==end);
+}
+
+test_suite* init_unit_test_suite(int, char* []) 
+{
+    test_suite* test = BOOST_TEST_SUITE("list type");
+    test->add(BOOST_TEST_CASE(&test_List));
+    test->add(BOOST_TEST_CASE(&test_list_const_iterator));
+    test->add(BOOST_TEST_CASE(&test_list_iterator));
+
+    return test;
+}
