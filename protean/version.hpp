3576ad143f6e2d0abe9aa31ac1c873562a90b664
Johan Ditmar <johanditmar@gmail.com>
2010-03-16 22:28:44 +0000

    Update version to 1.1 (close #28)

--- protean/version.hpp
+++ protean/version.hpp
@@ -11,13 +11,13 @@
 //  PROTEAN_VERSION / 100 % 1000 is the minor version
 //  PROTEAN_VERSION / 100000 is the major version
 
-#define PROTEAN_VERSION 100000
+#define PROTEAN_VERSION 100100
 
 //
 //  PROTEAN_LIB_VERSION must be defined to be the same as PROTEAN_VERSION
 //  but as a *string* in the form "x_y[_z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define PROTEAN_LIB_VERSION "1_00"
+#define PROTEAN_LIB_VERSION "1_01"
 
 #endif // PROTEAN_VERSION_HPP
