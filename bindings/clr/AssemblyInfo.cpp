9101dddbc7a7033a65b479cc37b3d1b948e7ad1a
Johan Ditmar <johanditmar@gmail.com>
2010-04-19 22:32:58 +0000

    Add AssemblyInfo to CLR projects

---
+++ bindings/clr/AssemblyInfo.cpp
@@ -0,0 +1,36 @@
+#include <protean/version.hpp>
+
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+
+//
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly:AssemblyTitleAttribute("Protean CLR Library")];
+[assembly:AssemblyDescriptionAttribute("")];
+[assembly:AssemblyConfigurationAttribute("")];
+[assembly:AssemblyCompanyAttribute("")];
+[assembly:AssemblyProductAttribute("Protean CLR Library")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c) 2010, Johan Ditmar, Karel Hruda, Paul O'Neill and Luke Stedman")];
+[assembly:AssemblyTrademarkAttribute("")];
+[assembly:AssemblyCultureAttribute("")];
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the value or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+
+[assembly:AssemblyVersionAttribute("1.1.0.0")];
+[assembly:AssemblyFileVersionAttribute("1.1.0.0")];
+
+[assembly:ComVisible(false)];
+
--- test/bindings/clr/AssemblyInfo.cs
+++ test/bindings/clr/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("vc9-test-protean-clr")]
+[assembly: AssemblyTitle("Protean CLR Tester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Protean")]
-[assembly: AssemblyProduct("vc9-test-protean-clr")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Protean CLR Tester")]
 [assembly: AssemblyCopyright("Copyright © Johan Ditmar, Karel Hruda, Paul O'Neill and Luke Stedman")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
