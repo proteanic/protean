fa70e9b8ce6d8094fe52ede3b66601cbfcec74ca
Johan Ditmar <johanditmar@gmail.com>
2011-06-28 13:09:09 +0000

    Update version for 1.4 release (re #TE-72)

--- protean/version.hpp
+++ protean/version.hpp
@@ -7,7 +7,7 @@
 #define PROTEAN_VERSION_HPP
 
 #define PROTEAN_MAJOR_VERSION 1
-#define PROTEAN_MINOR_VERSION 3
+#define PROTEAN_MINOR_VERSION 4
 #define PROTEAN_PATCH_VERSION 0
 
 //
@@ -15,13 +15,13 @@
 //  PROTEAN_VERSION / 100 % 1000 is the minor version
 //  PROTEAN_VERSION / 100000 is the major version
 
-#define PROTEAN_VERSION 100300
+#define PROTEAN_VERSION 100400
 
 //
 //  PROTEAN_LIB_VERSION must be defined to be the same as PROTEAN_VERSION
 //  but as a *string* in the form "x_y[_z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define PROTEAN_LIB_VERSION "1_03"
+#define PROTEAN_LIB_VERSION "1_04"
 
 #endif // PROTEAN_VERSION_HPP
