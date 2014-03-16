9d144658b62e8a7ed4d453dd0b69ec08a222e34e
johanditmar <johanditmar@gmail.com>
2014-03-16 18:24:17 +0000

    Icrement version number

--- protean/version.hpp
+++ protean/version.hpp
@@ -22,6 +22,6 @@
 //  but as a *string* in the form "x_y[_z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define PROTEAN_LIB_VERSION "1_06"
+#define PROTEAN_LIB_VERSION "1_07"
 
 #endif // PROTEAN_VERSION_HPP
